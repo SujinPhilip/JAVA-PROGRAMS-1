ae3e129563a7a2569b2e4f226db55c4476faf11b
SujinPhilip <73355949+SujinPhilip@users.noreply.github.com>
2020-10-29 21:29:27 +0530

    Update and rename P6.java to P06.java

--- P06.java
+++ P06.java
@@ -1,10 +1,12 @@
+//Program to find the factor of a given number
+
 import java.io.*;
 public class P6{
    public static void main(String[] args){
 	int i,n=10;
 	for(i=1;i<=n;i++){
 		if(n%i==0){
-		System.out.println("Factorial of "+n+" = "+i);
+		System.out.println("Factor of "+n+" = "+i);
 		}
 	}
 }
